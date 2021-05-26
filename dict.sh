b76c23bd22b398b05eaf6f2f937715d7e2d3ad39
SHISHIRA <Shishiras98@gmail.com>
2021-05-26 21:43:09 +0530

    ADDING TO DICTIONARY

---
+++ dict.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+declare -A dict
+echo "Please enter three number: "
+read a
+read b
+read c
+i=$(($a + $b * $c ))
+echo "result for expression a+b*c is $i"
+j=$(($a * $b + $c ))
+echo "result for expression a*b+c is $j"
+k=$(($c + $a / $b ))
+echo "result for expression c+a/b is $k"
+l=$(($a % $b + $c ))
+echo "result for expression a%b+c is $l"
+dict+=(["key1"]=$i ["key2"]=$j ["key3"]=$k ["key4"]=$l)
+echo "dict" ${dict[@]}
+for key in ${!dict[@]}
+do
+	echo $key=${dict[$key]}
+done
